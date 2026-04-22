2122ac7f8751032dc75c69c5ff792c3d24fdb8cb
Dmytro Datsenko <ddatsen1@stu.vistula.edu.pl>
2026-04-22 20:00:32 +0200

    Refactor theme and skills section toggling logic

--- zad_5/script.js
+++ zad_5/script.js
@@ -1,26 +1,21 @@
 
-// ZMIANA MOTYWU
 const themeBtn = document.getElementById('theme-btn');
-const themeLink = document.getElementById('theme-link');
-
-themeBtn.addEventListener('click', function() {
-    if (themeLink.getAttribute('href') === 'red.css') {
-        themeLink.setAttribute('href', 'green.css');
-    } else {
-        themeLink.setAttribute('href', 'red.css');
-    }
-});
-
-// POKAZ / UKRYJ UMIEJĘTNOŚCI
 const toggleBtn = document.getElementById('toggle-section-btn');
 const skillsSection = document.getElementById('skills-section');
 
-toggleBtn.addEventListener('click', function() {
-    if (skillsSection.style.display === 'none') {
-        skillsSection.style.display = 'block';
-        toggleBtn.textContent = 'Ukryj sekcję Umiejętności';
-    } else {
-        skillsSection.style.display = 'none';
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('theme-green');
+    document.body.classList.toggle('theme-red');
+});
+
+
+toggleBtn.addEventListener('click', () => {
+    skillsSection.classList.toggle('hidden');
+
+    if (skillsSection.classList.contains('hidden')) {
         toggleBtn.textContent = 'Pokaż sekcję Umiejętności';
+    } else {
+        toggleBtn.textContent = 'Ukryj sekcję Umiejętności';
     }
 });
